f7e9d5dfd8f6523df42ff402fce9060e4078c4a9
Marcelo <maduar@vivaldi.net>
2022-07-27 14:46:56 -0300

    Create read_a_spec.py

---
+++ src/spectrum_reader/read_a_spec.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 15:18:06 2020
+@author: marcelo
+"""
+
+from src.spec_class import Spec
+
+"""
+read_a_spec.py
+====================================
+Read a gammaa-ray spectrum
+"""
+
+# https://github.com/pypa/sampleproject
+
+a_spec_name = '../../Genie_Transfer/Filtros/2022/Cci/CCI1603-I.Chn'
+a_raw_spec = Spec(a_spec_name)
