de0ba1b5cdca80c6d675e55230daee0e5cef4190
terrabela <maduar@vivaldi.net>
2022-11-08 19:10:46 -0300

    Assemble general test Nov/2022

--- tests/test_spec_class.py
+++ tests/test_spec_class.py
@@ -1,20 +1,42 @@
+# from unittest import TestCase
+import sys
 from unittest import TestCase
-from src.spec_class import Spec
-
-
-# from src.specparms_class import SpecParms
-
 
 class TestSpec(TestCase):
     def setUp(self):
-        self.spec = Spec()
+        src_dir = sys.path[0] + '/src'
+        sys.path.insert(0, src_dir)
+        from spec_class import Spec
+        self.spec = Spec('data/some_spectra/CCI0202-I.Chn')
 
-# class TestSpec(TestCase):
-#    def test_curr_h_win(self):
-#        self.fail()#
-#
-#    def test_total_analysis(self):
-#        self.fail()#
-#
-#    def test_plot_graphics(self):
-#        self.fail()
+    def test_file_exists(self):
+        self.assertGreater(self.spec.gross_spec_ser_an.n_ch, 0)
+
+    def test_graphics(self):
+        from spec_graphics_class import GenericGraphics
+        self.assertTrue(True)
+
+    def test_plot(self):
+        from spec_graphics_class import GrossCountsGraphic
+        spec_graphic = GrossCountsGraphic(self.spec.f_name, self.spec.gross_spec_ser_an)
+
+    def test_fft_plot(self):
+        from scipy.fft import fft, ifft, fftfreq, fftshift
+        from spec_graphics_class import GenericGraphics
+        # fft_graphic = GenericGraphics('Qquer_coisa', [1,2,3], [4,5,6])
+        # Number of sample points
+        # N = 600
+        # sample spacing
+        # T = 1.0 / 800.0
+        # x = np.linspace(0.0, N * T, N, endpoint=False)
+        # y = np.sin(50.0 * 2.0 * np.pi * x) + 0.5 * np.sin(80.0 * 2.0 * np.pi * x)
+        # yf = fft(y)
+        # xf = fftfreq(N, T)[:N // 2]
+        # fft_ys = 2.0 / N * np.abs(yf[0:N // 2])
+        xf = self.spec.gross_spec_ser_an.x_s
+        fft_ys = fft(self.spec.gross_spec_ser_an.y_s)
+        fft_ys[2000:2096] = 0.0
+        fft_ys[1800:2296] = 0.0
+        ifft_ys = ifft(fft_ys)
+        ys_plot = abs(ifft_ys)
+        fft_graphic = GenericGraphics('Qquer_coisa', xf, ys_plot)
