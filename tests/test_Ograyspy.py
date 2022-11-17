bb2f5cf755c13991bc139695b613728e616a82ee
terrabela <maduar@vivaldi.net>
2022-11-17 17:08:54 -0300

    Changed in Windows; verify compatibility.

--- tests/test_Ograyspy.py
+++ tests/test_Ograyspy.py
@@ -1,43 +1,52 @@
-from unittest import TestCase
-from src.Ograyspy import Ograyspy
+import sys
+import os
 
+from unittest import TestCase
+from ograyspy_class import Ograyspy
 
 class TestOgrayspy(TestCase):
     def setUp(self):
         self.ogra = Ograyspy()
+        src_dir = sys.path[0] + '/src'
+        sys.path.insert(0, src_dir)
+        self.home_path = os.path.expanduser('~')
+        from spec_class import Spec
+        print(src_dir)
+        print(self.ogra.home_path)
+        print(self.ogra.spectra_path)
+        self.spec = Spec('data/some_spectra/CCI0202-I.Chn')
 
+    def test_file_exists(self):
+        self.assertGreater(self.spec.gross_spec_ser_an.n_ch, 0)
 
-class TestInit(TestOgrayspy):
-    def test_initial_speed(self):
-        self.assertEqual(self.ogra.speed, 0)
+    def test_graphics(self):
+        from spec_graphics_class import GenericGraphics
+        self.assertTrue(True)
 
-    def test_initial_odometer(self):
-        self.assertEqual(self.ogra.odometer, 0)
+    def test_plot(self):
+        from spec_graphics_class import GrossCountsGraphic
+        print(self.spec.f_name)
+        self.home_path = os.path.expanduser('~')
+        spec_graphic = GrossCountsGraphic(self.spec.f_name, self.spec.gross_spec_ser_an, self.home_path)
+        del spec_graphic
 
-    def test_initial_time(self):
-        self.assertEqual(self.ogra.time, 0)
-
-
-class TestAccelerate(TestOgrayspy):
-    def test_accelerate_from_zero(self):
-        self.ogra.accelerate()
-        self.assertEqual(self.ogra.speed, 5)
-
-    def test_multiple_accelerates(self):
-        for _ in range(3):
-            self.ogra.accelerate()
-        self.assertEqual(self.ogra.speed, 15)
-
-
-class TestBrake(TestOgrayspy):
-    def test_brake_once(self):
-        self.ogra.accelerate()
-        self.ogra.brake()
-        self.assertEqual(self.ogra.speed, 0)
-
-    def test_multiple_brakes(self):
-        for _ in range(5):
-            self.ogra.accelerate()
-        for _ in range(3):
-            self.ogra.brake()
-        self.assertEqual(self.ogra.speed, 10)
+    def test_fft_plot(self):
+        from scipy.fft import fft, ifft, fftfreq, fftshift
+        from spec_graphics_class import GenericGraphics
+        # fft_graphic = GenericGraphics('Qquer_coisa', [1,2,3], [4,5,6])
+        # Number of sample points
+        # N = 600
+        # sample spacing
+        # T = 1.0 / 800.0
+        # x = np.linspace(0.0, N * T, N, endpoint=False)
+        # y = np.sin(50.0 * 2.0 * np.pi * x) + 0.5 * np.sin(80.0 * 2.0 * np.pi * x)
+        # yf = fft(y)
+        # xf = fftfreq(N, T)[:N // 2]
+        # fft_ys = 2.0 / N * np.abs(yf[0:N // 2])
+        xf = self.spec.gross_spec_ser_an.x_s
+        fft_ys = fft(self.spec.gross_spec_ser_an.y_s)
+        fft_ys[2000:2096] = 0.0
+        fft_ys[1800:2296] = 0.0
+        ifft_ys = ifft(fft_ys)
+        ys_plot = abs(ifft_ys)
+        fft_graphic = GenericGraphics('Qquer_coisa', xf, ys_plot)
