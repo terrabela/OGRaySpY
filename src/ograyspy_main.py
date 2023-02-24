0026cdf976a7e32185622aba8cd6087b9c26bb91
terrabela <maduar@vivaldi.net>
2023-02-24 14:58:47 -0300

    Linking to RTD

--- src/ograyspy_main.py
+++ src/ograyspy_main.py
@@ -6,38 +6,64 @@ Created on Tue Nov 22 14:13:51 2022
 @author: marcelofm
 """
 
+from PySide2 import sys
+from PySide2.QtWidgets import QApplication, QMainWindow
+
 from ograyspy_class import Ograyspy
+from ui_ograyspy_main import Ui_MainWindow
 
-ogra = Ograyspy(batch_mode=False)
-print('Objeto ogra: Ograyspy criado.')
-print(f'ogra.info_plat: {ogra.info_plat}')
-print(f'ogra.info_mach: {ogra.info_mach}')
-print(f'ogra.info_syst: {ogra.info_syst}')
-print(f'ogra.info_node: {ogra.info_node}')
-print(f'ogra.home_path: {ogra.home_path}')
 
-to_be_found = 'Genie_Transfer'
-# to_be_found = 'some_spectra'
-print('\nogra.define_files_folder(to_be_found)')
-ogra.define_files_folder(to_be_found)
+def main2():
+    ogra = Ograyspy(batch_mode=False)
+    print('Objeto ogra: Ograyspy criado.')
+    print(f'ogra.info_plat: {ogra.info_plat}')
+    print(f'ogra.info_mach: {ogra.info_mach}')
+    print(f'ogra.info_syst: {ogra.info_syst}')
+    print(f'ogra.info_node: {ogra.info_node}')
+    print(f'ogra.home_path: {ogra.home_path}')
 
-# 2022-out-7: Excelente espectro para testes, tenho usado ultimamente:
-a_pattern = 'Si/SI2018/SI11318.Chn'
-# 2022-nov-16: outros espectros:
-# a_pattern = "Filtros/2022/Cci/CCI1603-I.Chn"
-# a_pattern = "Filtros/2022/Cci/CCI2302-I.Chn"
+    to_be_found = 'Genie_Transfer'
+    # to_be_found = 'some_spectra'
+    print('\nogra.define_files_folder(to_be_found)')
+    ogra.define_files_folder(to_be_found)
 
-# a_pattern = "Eso_non_existe.Chn"
+    # 2022-out-7: Excelente espectro para testes, tenho usado ultimamente:
+    a_pattern = 'Si/SI2018/SI11318.Chn'
+    # 2022-nov-16: outros espectros:
+    # a_pattern = "Filtros/2022/Cci/CCI1603-I.Chn"
+    # a_pattern = "Filtros/2022/Cci/CCI2302-I.Chn"
 
-print(f'\n\nogra.select_spectrum({a_pattern})')
-ogra.select_spectrum(a_pattern)
-print(f'A spec name: {ogra.a_spec_name}')
-print(f'Reduced file name: {ogra.reduced_f_name}')
+    # a_pattern = "Eso_non_existe.Chn"
 
-# AQUI: ativar gener_dataframe qdo estiver pronto.
-ogra.perform_total_analysis(peak_sd_fact=3.0, gener_dataframe=True)
-# print(ogra.a_spec.spec_pks_df)
+    print(f'\n\nogra.select_spectrum({a_pattern})')
+    ogra.select_spectrum(a_pattern)
+    print(f'A spec name: {ogra.a_spec_name}')
+    print(f'Reduced file name: {ogra.reduced_f_name}')
 
-ogra.call_graphics()
+    # AQUI: ativar gener_dataframe qdo estiver pronto.
+    ogra.perform_total_analysis(peak_sd_fact=3.0, gener_dataframe=True)
+    # print(ogra.a_spec.spec_pks_df)
 
-print('Terminated Ok.')
+    ogra.call_graphics()
+
+    print('Terminated Ok.')
+
+
+def main1():
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    # sys.exit(app.exec_())
+    app.exec_()
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.actionE_xit.triggered.connect(QApplication.instance().closeAllWindows)
+        self.action_Open_a_spectrum.triggered.connect(main2)
+
+if __name__ == '__main__':
+    main1()
+    # main2()
---
+++ src/ui_ograyspy_main.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ograyspy_mainnwkYcI.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.action_Open_a_spectrum = QAction(MainWindow)
+        self.action_Open_a_spectrum.setObjectName(u"action_Open_a_spectrum")
+        self.actionGenerate_report = QAction(MainWindow)
+        self.actionGenerate_report.setObjectName(u"actionGenerate_report")
+        self.actionP_lots = QAction(MainWindow)
+        self.actionP_lots.setObjectName(u"actionP_lots")
+        self.actionPandas_dataframe = QAction(MainWindow)
+        self.actionPandas_dataframe.setObjectName(u"actionPandas_dataframe")
+        self.actionE_xit = QAction(MainWindow)
+        self.actionE_xit.setObjectName(u"actionE_xit")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 21))
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menuFile.addAction(self.action_Open_a_spectrum)
+        self.menuFile.addAction(self.actionGenerate_report)
+        self.menuFile.addAction(self.actionP_lots)
+        self.menuFile.addAction(self.actionPandas_dataframe)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionE_xit)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"OGRaySpY", None))
+        self.action_Open_a_spectrum.setText(QCoreApplication.translate("MainWindow", u"&Open a spectrum", None))
+        self.actionGenerate_report.setText(QCoreApplication.translate("MainWindow", u"Generate &report", None))
+        self.actionP_lots.setText(QCoreApplication.translate("MainWindow", u"P&lots", None))
+        self.actionPandas_dataframe.setText(QCoreApplication.translate("MainWindow", u"Pandas &dataframe", None))
+        self.actionE_xit.setText(QCoreApplication.translate("MainWindow", u"E&xit", None))
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"&File", None))
+    # retranslateUi
+
