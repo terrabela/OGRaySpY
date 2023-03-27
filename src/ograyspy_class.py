3c7e82cf82332fa4bd7bd6052e496a854e3de95b
terrabela <maduar@vivaldi.net>
2023-03-27 20:20:42 -0300

    Refactoring graphics assembling

--- src/ograyspy_class.py
+++ src/ograyspy_class.py
@@ -5,6 +5,51 @@ import pickle
 import os
 import pandas as pd
 
+
+def select_spectrum_from_folder_list(reduced_names_file_list, files_list,
+                                     spectra_path, a_pattern='', random_spectrum=False):
+    # Select a random spectrum...
+    n_files = len(reduced_names_file_list)
+    if random_spectrum:
+        try:
+            a_spec_ind = randrange(n_files)
+            print('Random spec index: ', a_spec_ind)
+            a_spec_name = files_list[a_spec_ind]
+            reduced_f_name = reduced_names_file_list[a_spec_ind]
+            print('...and its name: ', a_spec_name)
+        except ValueError:
+            print('No random spectrum found...')
+        # ...or define it directly.
+    else:
+        print('Existing:')
+        achou = False
+        a_spec_ind = None
+        nomearq = ''
+        for i, j in enumerate(reduced_names_file_list):
+            if a_pattern in j:
+                achou = True
+                a_spec_ind = i
+                nomearq = j
+                break
+        if achou:
+            print(f'Achou! indice={a_spec_ind}, nomearq = {nomearq}')
+            a_spec_name = files_list[a_spec_ind]
+            reduced_f_name = reduced_names_file_list[a_spec_ind]
+
+        matching_spec_name = [i for i in spectra_path.glob(a_pattern)]
+        if len(matching_spec_name) != 0:
+            for i in matching_spec_name:
+                print('name: ', i)
+            a_spec_name = matching_spec_name[0]
+
+    print('==========================')
+    print('Final choices:')
+    print(f'spectra_path: {spectra_path}')
+    print(f'a_spec_name: {a_spec_name}')
+    print(f'reduced_f_name: {reduced_f_name}')
+    return a_spec_name, reduced_f_name
+
+
 class Ograyspy:
     files_list: list[str]
 
@@ -20,8 +65,8 @@ class Ograyspy:
         self.a_spec_ind = 0
         self.a_spec_name = ''
         self.reduced_f_name = ''
-        self.gross_counts_graphics = None
-        self.pks_regions_gros = None
+        # self.gross_counts_graphics = None
+        # self.pks_regions_gros = None
 
     def process_pickled_list(self, my_file):
         if os.path.isfile(my_file):  # if file exists we have already pickled a list
@@ -110,7 +155,6 @@ class Ograyspy:
         print(f'spectra_path: {self.spectra_path}')
         print(f'a_spec_name: {self.a_spec_name}')
         print(f'reduced_f_name: {self.reduced_f_name}')
-
 
 #   2023-Mar-22: AQUI: REFATORAR
 #    def perform_batch_analyses(self):
