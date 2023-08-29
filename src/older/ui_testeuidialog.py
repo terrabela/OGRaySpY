472ba8e542d0a87f2564a9f33e8ca3bf6d895ce7
Marcelo <maduar@vivaldi.net>
2023-08-29 17:56:17 -0300

    Delete src/older directory

--- src/older/ui_testeuidialog.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'testeuidialog.ui'
-#
-# Created: Mon Feb  3 16:21:40 2014
-#      by: PyQt4 UI code generator 4.10.3
-#
-# WARNING! All changes made in this file will be lost!
-
--- src/older/untitled1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 28 17:47:35 2016
-
-@author: marcelo
-"""
-
-for i in range(30,25,-1):
-    print(i)
